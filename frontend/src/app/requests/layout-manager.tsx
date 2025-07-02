d6d87eec36816861df50b63abcd4453bda603d6d
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-07-02 23:17:52 +0800

    docs(frontend): elaborate on the logic behind the force show of <MessageView/> on larger screens

--- frontend/src/app/requests/layout-manager.tsx
+++ frontend/src/app/requests/layout-manager.tsx
@@ -17,7 +17,30 @@ export default function LayoutManager({ className, children }: props) {
     state => state
   )
 
+  /*
+  On small screens the inbox and message-view will toggle between
+  one another. The user can click on a message on their inbox which
+  switches to message-view. Message-view will then have a back button
+  to go back to the inbox so they can view other messages. On larger
+  screens the two will be shown together in separate columns.
+
+  The initial changing of two columns to one column is obviously done
+  using css breakpoints, and the switching on small screens will be 
+  passed through context to dictate what to show on small screens: 
+  is the message-view active or not?
+
+  A bug arises from this where if you were to click the back button on
+  message-view, it sets isMessageViewActive to false hiding it and 
+  and showing the inbox. But what if you increase the window size after?
+  The message-view will be hidden. So you have to force-set 
+  isMessageViewActive to true on larger screen sizes. This is done in 
+  the useEffect() below.
+  */
   useEffect(() => {
+    /*
+    This project uses tailwindcss v4 which no longer uses tailwind.config.ts and removed the resolveCOnfig() function to turn the tailwind config to a
+    flat object. So we need to get the computed styles in the browser.
+    */
     const styles = getComputedStyle(document.documentElement)
 
     const fontSize = parseFloat(styles.fontSize)
@@ -27,13 +50,16 @@ export default function LayoutManager({ className, children }: props) {
     const checkIsMediumScreen = () => {
       const isMedium = window.innerWidth > breakpointInPx
 
+      /* Force show the messages-view on large screens */
       if (isMedium) {
         setIsActive(true)
       }
     }
 
+    /* Initial Check */
     checkIsMediumScreen()
 
+    /* Check every resize */
     window.addEventListener('resize', checkIsMediumScreen)
     return () => window.removeEventListener('resize', checkIsMediumScreen)
   }, [])
