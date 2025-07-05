4e86ace558091c015964541821739cb6cf7a821c
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-07-05 18:58:22 +0800

    feat(frontend): redirect url path '/' to '/chats'

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/chats',
+        permanent: true
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       new URL(
