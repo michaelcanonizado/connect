1fdbfa4043aec9d941cd15f866b7d3fcfa215436
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-07-05 21:47:31 +0800

    refactor(frontend): since login form is in the landing page, move contents of /login to /: authentication will be done using OAuth so no separate login/sign-up form is needed. Once the  user is authenticated, they will be redirected to /chats

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/chats',
-        permanent: true
-      }
-    ]
-  },
   images: {
     remotePatterns: [
       new URL(
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,42 @@
-import { TextHeading } from '@/components/text'
+import React from 'react'
+import { TextBody, TextDisplay } from '@/components/text'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className=''>
-      <TextHeading>Connect</TextHeading>
+    <div className='flex min-h-screen flex-col items-center justify-center gap-8'>
+      <div className='text-muted-400 flex max-w-[600px] flex-col gap-4'>
+        <div className=''>
+          <TextDisplay className=''>
+            More than Chat - It&apos;s{' '}
+            <span className='text-primary'>Connect</span>
+            ion
+          </TextDisplay>
+        </div>
+        <div className=''>
+          <TextBody className='text-[18px] leading-[26px] font-[500] tracking-[-0.01em]'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            dignissim nulla eget tortor rhoncus porttitor.
+          </TextBody>
+        </div>
+      </div>
+      <div className=''>
+        <Button
+          variant='outline'
+          className='border-primary h-min w-min px-[60px] py-[8px] hover:cursor-pointer'
+        >
+          <div className='relative size-[24px]'>
+            <Image
+              className='object-contain object-center'
+              src='icons/google.svg'
+              alt='Sign in with Google'
+              fill
+            />
+          </div>
+          <TextBody>Sign in with Google</TextBody>
+        </Button>
+      </div>
     </div>
   )
 }
