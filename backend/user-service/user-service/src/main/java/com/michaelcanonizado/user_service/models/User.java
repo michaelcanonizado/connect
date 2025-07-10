8e19c3a1d9c87010e710638b8273e5d9dfbf527b
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-07-10 22:22:36 +0800

    feat(backend/user-service): add User model

---
+++ backend/user-service/user-service/src/main/java/com/michaelcanonizado/user_service/models/User.java
@@ -0,0 +1,106 @@
+package com.michaelcanonizado.user_service.models;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String bio;
+    private String profileUrl;
+
+    private boolean isOnline;
+    private Instant lastSeenAt;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdAt;
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    public User() {
+    }
+
+    public User(String name, String username, String bio, String profileUrl, boolean isOnline, Instant lastSeenAt) {
+        this.name = name;
+        this.username = username;
+        this.bio = bio;
+        this.profileUrl = profileUrl;
+        this.isOnline = isOnline;
+        this.lastSeenAt = lastSeenAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public Instant getLastSeenAt() {
+        return lastSeenAt;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    public void setLastSeenAt(Instant lastSeenAt) {
+        this.lastSeenAt = lastSeenAt;
+    }
+}
