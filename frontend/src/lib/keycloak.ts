267fae5a9a0a38cbbf543109f51f1286f439476d
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-07-31 21:17:43 +0800

    chore(frontend): remove /frontend as project will migrate from NextJS to vanila ReactJS

--- frontend/src/lib/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js'
-
-const keycloak = new Keycloak({
-  url: 'http://localhost:8081',
-  realm: 'connect',
-  clientId: 'frontend-web'
-})
-
-export default keycloak
