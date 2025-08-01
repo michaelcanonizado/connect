6ffd03fe04d7c610d2556f9af7fd6e17dbc48098
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-01 20:03:50 +0800

    feat(frontend): add 'get started' button to '/' to redirect to login form

--- frontend/src/layouts/app-layout.tsx
+++ frontend/src/layouts/app-layout.tsx
@@ -2,12 +2,12 @@ import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
 	return (
-		<div className="grid grid-cols-3 h-screen">
-			<div className="bg-red-400">Sidebar</div>
+		<div className="grid grid-cols-3 h-screen divide-x">
+			<div className="">Sidebar</div>
 			<div>
 				<Outlet />
 			</div>
-			<div className="bg-red-400">Convo view</div>
+			<div className="">Convo view</div>
 		</div>
 	);
 }
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-	return <div>Home</div>;
+	const redirectToLogin = () => {
+		console.log('Redirecting to login....');
+	};
+
+	return (
+		<div>
+			<h1 className="text-lg">Home</h1>
+			<button onClick={redirectToLogin}>Get Started</button>
+		</div>
+	);
 }
