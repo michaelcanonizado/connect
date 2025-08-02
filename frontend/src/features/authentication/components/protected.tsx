ef08a149dea32532eb9bc7d3cd5ff5d8ebee9925
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-02 21:43:21 +0800

    fix(frontend/authentication): use isAuthenticated condition in useEffect to get consistent behavior

--- frontend/src/features/authentication/components/protected.tsx
+++ frontend/src/features/authentication/components/protected.tsx
@@ -1,17 +1,21 @@
+import paths from '@/app/paths';
 import { useAuthentication } from '@/store/useAuthentication';
 import { useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Protected({ children }: ComponentChildrenProp) {
 	const isAuthenticated = useAuthentication((state) => state.isAuthenticated);
+	const login = useAuthentication((state) => state.login);
 
 	useEffect(() => {
 		console.log('<Protected/>: isAuthenticated: ', isAuthenticated);
-	}, []);
 
-	if (!isAuthenticated) {
-		return <Navigate to={'/'} replace />;
-	}
+		if (!isAuthenticated) {
+			login();
+		}
+	}, [isAuthenticated]);
+
+	if (!isAuthenticated) return null;
 
 	return children;
 }
