fa63c9c885efb9494aa1639a731b7df9222b7515
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-11 20:08:37 +0800

    fix(frontend): move initial data fetching to <Protected/> so it runs on all protected routes even on refresh

--- frontend/src/app/pages/(authentication)/callback.tsx
+++ frontend/src/app/pages/(authentication)/callback.tsx
@@ -1,29 +1,13 @@
 import paths from '@/app/paths';
-import { useUser } from '@/store/useUser';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Callback() {
 	const navigate = useNavigate();
-	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		const initialize = async () => {
-			try {
-				await Promise.all([useUser.getState().fetchUser()]);
-			} catch (error) {
-				console.error('Something went wrong! ', error);
-			} finally {
-				setIsLoading(false);
-			}
-			navigate(paths.user.chats.root.path);
-		};
-
-		setIsLoading(true);
-		initialize();
+		navigate(paths.user.chats.root.path);
 	}, []);
 
-	if (isLoading) {
-		return <div>Logging you in...</div>;
-	}
+	return <div>Logging you in...</div>;
 }
--- frontend/src/features/authentication/components/protected.tsx
+++ frontend/src/features/authentication/components/protected.tsx
@@ -1,19 +1,40 @@
+import paths from '@/app/paths';
 import { useAuthentication } from '@/store/useAuthentication';
-import { useEffect } from 'react';
+import { useUser } from '@/store/useUser';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Protected({ children }: ComponentChildrenProp) {
 	const isAuthenticated = useAuthentication((state) => state.isAuthenticated);
 	const login = useAuthentication((state) => state.login);
+
+	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		console.log('<Protected/>: isAuthenticated: ', isAuthenticated);
 
 		if (!isAuthenticated) {
 			login();
+			return;
 		}
+
+		const initialize = async () => {
+			try {
+				await Promise.all([useUser.getState().fetchUser()]);
+			} catch (error) {
+				console.error('Something went wrong! ', error);
+			} finally {
+				setIsLoading(false);
+			}
+			navigate(paths.user.chats.root.path);
+		};
+
+		setIsLoading(true);
+		initialize();
 	}, [isAuthenticated, login]);
 
 	if (!isAuthenticated) return null;
-
+	if (isLoading) return <h1>Loading...</h1>;
 	return children;
 }
