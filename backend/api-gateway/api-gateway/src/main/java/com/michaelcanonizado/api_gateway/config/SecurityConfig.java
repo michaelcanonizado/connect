be319a0e04b288d78c4961212a29d63b85633033
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2025-08-11 20:58:32 +0800

    fix(backend/api-gateway): ename CORS and allow all HTTP OPTIONS without authentication to allow preflight from browsers

--- backend/api-gateway/api-gateway/src/main/java/com/michaelcanonizado/api_gateway/config/SecurityConfig.java
+++ backend/api-gateway/api-gateway/src/main/java/com/michaelcanonizado/api_gateway/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.michaelcanonizado.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,8 +18,13 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(customizer -> customizer.disable())
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyExchange()
+                        .authenticated()
+                )
                 .oauth2ResourceServer(oath2 -> oath2.jwt(Customizer.withDefaults()));
         return http.build();
     }
